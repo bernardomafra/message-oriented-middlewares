b528b18b2a629f258797c4a132861ddb6b19671a
Bernardo Mafra <bernardo.mafra@queimadiaria.com.br>
2021-09-30 23:26:14 -0300

    initial files

---
+++ rabbit-mq/producer/index.js
@@ -0,0 +1,30 @@
+const amqp = require('amqplib/callback_api');
+
+function init(error, connection) {
+  if (error) throw error;
+
+  connection.createChannel(function (channelError, channel) {
+    if (channelError) throw channelError;
+
+    const queue = 'hello_world';
+    const msg = process.argv.slice(2).join(' ') || 'EMPTY';
+
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    channel.sendToQueue(queue, Buffer.from(msg), {
+      persistent: true,
+    });
+
+    console.log(" [x] Sent '%s'", msg);
+
+    // closeConnection
+    setTimeout(function () {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  });
+}
+
+amqp.connect('amqp://localhost', init);
